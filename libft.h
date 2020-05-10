a32640fa166e498fb051c7ad5464877c5f99635e
digiTALker <d_talker@mail.ru>
2020-05-10 23:38:30 +0300

    bzero

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cmyrtle <cmyrtle@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 22:32:00 by cmyrtle           #+#    #+#             */
-/*   Updated: 2020/05/09 19:06:14 by cmyrtle          ###   ########.fr       */
+/*   Updated: 2020/05/10 23:34:29 by cmyrtle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@
 void	ft_putchar_fd(char c, int fd);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
-void	bzero(void *s, size_t n);
-void	*memmove(void *dest, const void *src, size_t n);
-int		memcmp(const void *s1, const void *s2, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
